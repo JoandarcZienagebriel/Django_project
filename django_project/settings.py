42d1505758eaefe9def032cb763347567086443e
Joandarc <joandarczienagebriel@gmail.com>
2026-01-17 09:38:49 +0100

    Fix database configuration for Render

--- django_project/settings.py
+++ django_project/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default="sqlite:///" + str(BASE_DIR / "db.sqlite3"),
-        conn_max_age=600,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
