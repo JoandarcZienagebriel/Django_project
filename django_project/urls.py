8037ac0408f6e78051083eb1830a71ba4b604cdb
Joandarc <joandarczienagebriel@gmail.com>
2026-01-17 13:40:16 +0100

    Fix homepage / routing to blog app

--- django_project/urls.py
+++ django_project/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
-    path('blog/', include('blog.urls')), 
+    path('', include('blog.urls')), 
     path('crud/', include('crud.urls')),
 
 
