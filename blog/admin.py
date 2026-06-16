96634c6ec4c8a4803c3e2da466794d8888552e77
Joandarc Zienagebriel <joandarczienagebriel@gmail.com>
2026-06-16 15:40:10 +0300

    change_in_style

---
+++ blog/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from .models import BookReview, Comment, Europe, CommentEurope, Message
+
+class BookReviewAdmin(admin.ModelAdmin):
+    list_display=[
+        "author",
+        "title",
+        "date",
+        "body",
+        'image',
+    ]
+    
+class EuropeAdmin(admin.ModelAdmin):
+    list_display=[
+        "author",
+        "title",
+        "date",
+        "body",
+        'image',
+    ]
+class MessageAdmin(admin.ModelAdmin):
+    list_display=[
+        "First_name",
+        "Last_name",
+        "Email",
+        "Subject",
+        "Message",
+]
+
+
+admin.site.register(BookReview, BookReviewAdmin)
+admin.site.register(Comment)
+admin.site.register(Europe, EuropeAdmin)
+admin.site.register(CommentEurope)
+admin.site.register(Message, MessageAdmin)
+# Register your models here.
+ 
+
+# Register your models here.
