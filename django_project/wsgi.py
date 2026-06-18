3b05f62b63bb69f75f53e8f99d8ebe0ffabb7839
Joandarc Zienagebriel <joandarczienagebriel@gmail.com>
2026-06-19 01:27:13 +0300

    fix cloudinary storage

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -6,15 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
 application = get_wsgi_application()
-from django.conf import settings
-if settings.DEBUG is False:
-    from django_project.settings import create_default_admin
-    create_default_admin()
