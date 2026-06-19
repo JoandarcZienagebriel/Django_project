0a3eb48f198929ad83d74c340586887d4c31bf10
Joandarc Zienagebriel <joandarczienagebriel@gmail.com>
2026-06-19 09:40:40 +0300

    remove_j-query_file

--- django_project/settings.py
+++ django_project/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
 ]
 
 CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": "dycacj173",
-    "API_KEY": "271825921642587",
-    "API_SECRET": "crAm5Wl5qQVLQxMSGnned_CRjJs",
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
 }
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
